078a8dc8eea590570a8b823dc3f4b293046d694e
Chatur Jodhani <chatur.iosdeveloper@gmail.com>
2022-03-24 14:54:32 +0530

    initial commit

---
+++ TipCalculator/TipCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorApp.swift
+//  TipCalculator
+//
+//  Created by Chirag's on 22/03/22.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
